2dfc3be156c772eb66823a0d3614d33694fcd1c1
B0bUy <153715633+B0bUy@users.noreply.github.com>
2026-03-04 21:02:11 +0800

    Added Hub | OnConnectedAsync

---
+++ Talaorasan.Server/RealTime/Connection/IConnectionManager.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Concurrent;
+
+namespace Talaorasan.Server.RealTime.Connection
+{
+    public interface IConnectionManager
+    {
+        void AddConnection(string deviceId, string connectionId);
+        void RemoveConnection(string connectionId);
+        List<string> GetConnections(string userId);
+    }
+
+    public class ConnectionManager
+    {
+        private readonly ConcurrentDictionary<string, List<string>> _deviceConnections = new();
+        private readonly ConcurrentDictionary<string, string> _connectionToDevice = new();
+
+        public void AddConnection(string deviceId, string connectionId)
+        {
+            _connectionToDevice[connectionId] = deviceId;
+
+            _deviceConnections.AddOrUpdate(deviceId,
+                _ => new List<string> { connectionId },
+                (_, list) =>
+                {
+                    lock (list) list.Add(connectionId);
+                    return list;
+                });
+        }
+
+        public void RemoveConnection(string connectionId)
+        {
+            if (_connectionToDevice.TryRemove(connectionId, out var deviceId) &&
+                _deviceConnections.TryGetValue(deviceId, out var list))
+            {
+                lock (list)
+                {
+                    list.Remove(connectionId);
+                    if (list.Count == 0)
+                        _deviceConnections.TryRemove(deviceId, out _);
+                }
+            }
+        }
+
+        public List<string> GetConnections(string userId)
+        {
+            return _deviceConnections.TryGetValue(userId, out var list) ? list : new List<string>();
+        }
+    }
+}
--- Talaorasan.Server/RealTime/TalaorasanHub.cs
+++ Talaorasan.Server/RealTime/TalaorasanHub.cs
@@ -1,12 +1,20 @@
 ﻿using Microsoft.AspNetCore.SignalR;
+using Talaorasan.Server.RealTime.Connection;
 
 namespace Talaorasan.Server.RealTime
 {
     public class TalaorasanHub : Hub
     {
+        private readonly IConnectionManager _manager;
+        public TalaorasanHub(IConnectionManager manager)
+        {
+            _manager = manager;
+        }
         public override Task OnConnectedAsync()
         {
-            var stationId = Context.GetHttpContext()?.Request.Query["stationId"].ToString();
+            var deviceId = Context.GetHttpContext()?.Request.Query["deviceId"].ToString();
+            if (!string.IsNullOrEmpty(deviceId))
+                _manager.AddConnection(deviceId, Context.ConnectionId);
             return base.OnConnectedAsync();
         }
     }
